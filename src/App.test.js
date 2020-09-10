630d8101220dafa9431e445937698ce4a58df0fd
Vishnu <vishnu.m@digital.datamatics.com>
2020-09-11 01:47:13 +0530

    serv 4

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Hello react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello Sammy/i);
   expect(linkElement).toBeInTheDocument();
 });
